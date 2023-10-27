6429c3a0af9487653fc5423c5ac4d8d8d1834948
Fran <fifame.francesca@gmail.com>
2023-10-27 16:01:16 +0200

    :star: Add Entities

---
+++ SpaceEngine/Engine/Entity/Bullet.cpp
@@ -0,0 +1,18 @@
+/*
+** G-TECH PROJECT, 2023
+** SpaceEngine
+** File description:
+** Bullet
+*/
+
+#include "Bullet.hpp"
+
+namespace SpaceEngine {
+    Bullet::Bullet(std::string name) : Entity(BULLET, name)
+    {
+    }
+
+    Bullet::~Bullet()
+    {
+    }
+}
---
+++ SpaceEngine/Engine/Entity/Character.cpp
@@ -0,0 +1,18 @@
+/*
+** G-TECH PROJECT, 2023
+** SpaceEngine
+** File description:
+** Character
+*/
+
+#include "Character.hpp"
+
+namespace SpaceEngine {
+    Character::Character(std::string name) : Entity(CHARACTER, name)
+    {
+    }
+
+    Character::~Character()
+    {
+    }
+}
