754cd1b3d267e980e5b52b12c8a9a9685971073b
Fran <fifame.francesca@gmail.com>
2023-10-27 16:01:16 +0200

    :star: Add Entities

---
+++ SpaceEngine/Engine/Entities.hpp
@@ -0,0 +1,15 @@
+/*
+** G-TECH PROJECT, 2023
+** SpaceEngine
+** File description:
+** Entities
+*/
+
+#ifndef ENTITIES_HPP_
+#define ENTITIES_HPP_
+
+#include "Entity/Entity.hpp"
+#include "Entity/Bullet.hpp"
+#include "Entity/Character.hpp"
+
+#endif /* !ENTITIES_HPP_ */
