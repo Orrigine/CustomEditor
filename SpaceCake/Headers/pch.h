387a1c0be29ab1ab51d33280ff5a2f2d31453415
Tom LE GROS <freezekaz.dement@gmail.com>
2023-10-30 10:07:20 +0100

    :wrench: Update AND FIX :japanese_ogre: :skull:
    FIX

--- SpaceCake/Headers/pch.h
+++ SpaceCake/Headers/pch.h
@@ -4,6 +4,7 @@
 #include "Headers/Render.h"
 #include "Common/Headers/d3dutil.h"
 #include <iostream>
+#pragma comment (lib, "SpaceEngine.lib")
 
 #define MAX_NAME_STRING 256
 #define HInstance() GetModuleHandle(NULL)
